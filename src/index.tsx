d5d7b2afa9c2db4f91666eefce34889d33b6c56b
Geeth Wishkamal <41125548+geethwish@users.noreply.github.com>
2022-07-11 20:31:43 +0530

    basic folder structure added

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+} from "react-router-dom";
+
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
@@ -10,11 +16,13 @@ const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </Provider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
